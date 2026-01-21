287e1d9c36893e44204a437084e9e6687de796d4
Rishabsingh7 <rishabsingh78089@gmail.com>
2026-01-22 00:24:09 +0530

    add two pages cancellation-refund and privacy-polocy pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { OurProductsComponent } from './pages/our-products/our-products.component';
+import { CancellationRefundComponent } from './pages/cancellation-refund/cancellation-refund.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'our-products', component: OurProductsComponent},
+  { path: 'cancellation-refund', component: CancellationRefundComponent},
+  { path: 'privacy-policy',component:PrivacyPolicyComponent},
   { path: '**', redirectTo: '' }
 ];
